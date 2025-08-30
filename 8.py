4ac73f5ee06c81bf2f72a79192d39977277f5781
vtn-hz <juaniyvalen1234@gmail.com>
2025-08-29 23:34:26 -0300

    feat: full implemented 8

---
+++ 8.py
@@ -0,0 +1,23 @@
+import math
+
+def calculateI(pi: float) -> float:
+    return math.log2(1/pi); 
+
+def calculateH(P: list) -> float:
+    return sum([
+        pi*calculateI(pi) 
+        for pi in P
+    ])
+
+def getBinaryP( w: float ) -> list:
+    return [w, 1 - w]
+
+def main():
+    rawData = float(input("w: "))
+    P = getBinaryP( w )
+    
+
+    print( [calculateI(pi) for  pi in P] )
+    print( calculateH(P) )
+
+main()
