aff26234db60697e6e97260178e0cbe523f084df
vtn-hz <juaniyvalen1234@gmail.com>
2025-08-29 21:47:52 -0300

    ref: 3

--- 3.py
+++ 3.py
@@ -1,32 +1,20 @@
 import math
 
-def calculateI(p):
-    return math.log2(1/p); 
+def calculateI(pi: float) -> float:
+    return math.log2(1/pi); 
 
-def getIDistribution(P):
-    return [calculateI(item) for item in P]
+def calculateH(P: list) -> float:
+    return sum([
+        pi*calculateI(pi) 
+        for pi in P
+    ])
 
-def calculateH(P):
-    return sum ([ 
-        Pi * Ii
-        for Pi, Ii in  
-        zip(P, getIDistribution(P))
-    ]); 
+def main():
+    rawData = input("list: ")
+    P = eval(rawData)
+    
 
-"""
-def calculateH(P):
-    H = 0
-    i = 0
+    print( [calculateI(pi) for  pi in P] )
+    print( calculateH(P) )
 
-    for item in getIDistribution(p):
-        H += P[i] * item
-        i += 1;
-
-"""
-
-# P(1) = 1/9, P(2) = 1/6, P(3) = 1/9, P(4) = 1/9, P(5) = 1/6, P(6) = 1/3
-
-l = [1/9, 1/6, 1/9, 1/9, 1/6, 1/3]
-print(l)
-print( getIDistribution(l) )
-print( calculateH(l) )
+main()
