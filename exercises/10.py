91b29dd206ab5854e238d41a817706fe35c65d90
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-02 23:01:59 -0300

    feat: 10

--- exercises/10.py
+++ exercises/10.py
@@ -1,34 +1,27 @@
 
 def generateExtensions(alf: list, prob: list, n: int):
-    # ... 
-
-def getOcurrences( phrase: str ) -> dict:
-    occurrences = {}
-
-    for si in phrase:
-        if not (si in occurrences):
-            occurrences[si] = 0
-        occurrences[si] += 1
+    if n == 1:
+        return alf, prob
     
-    return occurrences
+    P = []
+    S = []
 
-'''
-@return dictionary: { letra: porcentaje aparicion } 
-'''
-def buildS ( source: str ) -> dict:
-    occurrences = getOcurrences( source )
-    return { si:cant/len(source) for si, cant in occurrences.items() }
+    prevS, prevP = generateExtensions(alf, prob, n-1)
+    
+    for i, phrase  in enumerate(prevS):
+        for j, symbol in enumerate(alf):
+            S.append( phrase + symbol )
+            P.append( prevP[i] * prob[j] )
+    return S, P 
 
 def main():
     S = ['1', '2', '3', '4', '5', '6']
     Sp = [1/6]*6
     n = int( input('n: ') )
 
-    result = []
+    extensions = generateExtensions(S, Sp, n)
 
-    print( generateExtensions(S, Sp, result, [], 0, n) )
+    print(dict(zip(extensions[0], extensions[1])))
     print("\n")
-
-
 
 main()
