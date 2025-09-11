cff1dd0b929b9fc3b642a4daf355ff70410f977d
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-10 21:54:55 -0300

    5 & 6

---
+++ tp3/6.py
@@ -0,0 +1,66 @@
+def getSubfix(code: str, prefix: str):
+    return code.replace(prefix, "", 1)
+
+
+def hasAnyEqual(stack, S: set):
+    return S in stack
+
+
+
+def sardinasPatterson (S: set):
+    stack = []
+    stack.append( set (S))
+    
+    while (True):
+        Si = set()
+        for x in S:
+            for y in stack[-1]:
+                if x == y:
+                    continue
+                
+                subfix = False
+                if x.startswith(y):
+                    subfix = getSubfix(x, y)
+                elif y.startswith(x):
+                    subfix = getSubfix(y, x)
+                
+                if (subfix):
+                    if (subfix in S):
+                        return False
+
+                    Si.add(subfix)
+
+        if ( hasAnyEqual(stack, Si) ):     
+            return True
+        
+        stack.append( Si )
+
+def hasRepeated(codes: list):
+    return len(codes) != len( set(codes) )
+
+def hasSubfixes(codes: list):
+    for x in codes:
+        for y in codes: 
+            if x != y and y.startswith(x):
+                return True
+
+    return False
+
+def main():
+    val = eval( input("S: ") )
+
+    if hasRepeated(val):
+        print("Es bloque")
+        return 
+
+    if not hasSubfixes(val):
+        print("Es instantaneo")
+        return
+    
+    if sardinasPatterson(set(val)):
+        print("Es univoco")
+    else:
+        print("Es no-singular")
+
+main()
+
