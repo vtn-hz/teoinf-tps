91c74666829cac431f3601102dfa75fcdb7bcba5
vtn-hz <juaniyvalen1234@gmail.com>
2025-09-22 22:55:23 -0300

    feat: fullset zero matrix

--- utils/matrix.py
+++ utils/matrix.py
@@ -6,3 +6,7 @@ def getMatrixTraspuesta(M: list[list]) -> list[list]:
         for j in range(len(M)):
             result[i].append(M[j][i])  
     return result
+
+def getMatrixZeros(filas: int, columnas: int) -> list[list[int]]:
+    return [[0 for _ in range(columnas)] for _ in range(filas)]
+
