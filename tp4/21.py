5e75b73a17ed500265c1d2a1ddaf3cb141a1b821
valentinoherpas <86085276+vtn-hz@users.noreply.github.com>
2025-10-14 19:44:05 -0300

    feat: 21

---
+++ tp4/21.py
@@ -0,0 +1,80 @@
+
+
+def hamming (C: list[str]) -> int:
+    
+    if len(C) <= 1:
+        raise Exception("No hay cantidad de codigos suficientes") 
+
+    _min = []
+    for _ in range(0, len(C)):
+        dif_amount = 0 
+
+        for i in range(0, len(C)):
+            for j in range(i+1, len(C)):
+                if i != j:
+                    for k in range(0, len(C[i])):
+                        dif_amount += int(C[i][k] != C[j][k]) 
+                    _min.append( dif_amount )
+
+    return min( _min )
+
+def erroresDetectables( C: list[str] ) -> int:
+    return hamming(C) - 1
+
+def erroresCorregibles( C: list[str] ) -> float:
+    return (hamming(C) - 1) // 2
+
+
+def main(): 
+    C1 = ['00', '01', '10', '11']
+    C2 = ['000', '100', '101', '111']
+    C3 = ['0000', '0011', '1010', '0101']
+    C4 = ['0100100', '0101000', '0010010', '0100000']
+    C5 = ['0100100', '0010010', '0101000', '0100001']
+    C6 = ['0110000', '0000011', '0101101', '0100110']
+
+
+    print( "hamming:" + str(hamming(C1)) )
+    print( "errores detectables:" + str(erroresDetectables( C1 )) )
+    print( "errores corregibles:" + str(erroresCorregibles( C1 )) )
+
+    print( "hamming:" + str(hamming(C2)))
+    print( "errores detectables:" + str(erroresDetectables( C2 )) )
+    print( "errores corregibles:" + str(erroresCorregibles( C2 )) )
+
+
+    print( "hamming:" + str(hamming(C3)))
+    print( "errores detectables:" + str(erroresDetectables( C3 )))
+    print( "errores corregibles:" + str(erroresCorregibles( C3 )))
+
+    print( "hamming:" + str(hamming(C4)) )
+    print( "errores detectables:" + str(erroresDetectables( C4 )) )
+    print( "errores corregibles:" + str(erroresCorregibles( C4 )) )
+
+    print( "hamming:" + str(hamming(C5)))
+    print( "errores detectables:" + str(erroresDetectables( C5 )) )
+    print( "errores corregibles:" + str(erroresCorregibles( C5 )) )
+
+
+    print( "hamming:" + str(hamming(C6)))
+    print( "errores detectables:" + str(erroresDetectables( C6 )))
+    print( "errores corregibles:" + str(erroresCorregibles( C6 )))
+
+
+ 
+main()
+
+'''
+
+Color Código 1 Código 2 Código 3
+Rojo 00 000 0000
+Amarillo 01 100 0011
+Verde 10 101 1010
+Azul 11 111 0101
+
+Color Código 1 Código 2 Código 3
+Rojo 0100100 0100100 0110000
+Amarillo 0101000 0010010 0000011
+Verde 0010010 0101000 0101101
+Azul 0100000 0100001 0100110
+'''
