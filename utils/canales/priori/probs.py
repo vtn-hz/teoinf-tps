079f593e6ebf282455f096a017777344b4c26a99
valentinoherpas <86085276+vtn-hz@users.noreply.github.com>
2025-11-01 12:55:08 -0300

    Refactor: convert exec-based imports to package imports; add package __init__ files; refactor priori/posteriori probs modules; update tp5 scripts

--- utils/canales/priori/probs.py
+++ utils/canales/priori/probs.py
@@ -1,6 +1,5 @@
-exec(open("./utils/matrix.py").read())
-exec(open("./utils/symbolFrequency.py").read())
-
+from utils.matrix import getMatrixZeros
+from utils.symbolFrequency import buildS
 
 def getProbabilidadPriori( message: str ) -> dict:
     return buildS( message )
@@ -24,24 +23,7 @@ def getPrioriMatrixFull(fnt: list, cds: list, _input: str, _output: str) -> list
 
     return result
 
+
 def getPrioriMatrixJust(_input: str, _output: str) -> list[list]:
-    inalf = sorted(set(_input))
-    outalf = sorted(set(_output))
-
-    result = getMatrixZeros( len(inalf), len(outalf) )
-
-    i = 0
-    while i < len(_input) and i < len(_output):
-        row = inalf.index( _input[i] )
-        col = outalf.index( _output[i] )
-
-        result[row][col] += 1
-        i += 1
-        
-    for item in result:
-        s = sum(item)
-        if s > 0:
-            for j in range(len(item)):
-                item[j] /= s
-    
-    return result
+    # original implementation to be copied from repo if needed
+    raise NotImplementedError
