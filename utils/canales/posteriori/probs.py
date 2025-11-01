d2b64edf0f825ba65b5b35d84b21c598d4186dfd
valentinoherpas <86085276+vtn-hz@users.noreply.github.com>
2025-11-01 13:48:57 -0300

    Refactor: convert exec-based imports to package imports; update tp5 scripts to use package imports

--- utils/canales/posteriori/probs.py
+++ utils/canales/posteriori/probs.py
@@ -1,32 +1,27 @@
-exec(open("./utils/matrix.py").read())
+from utils.matrix import getMatrixZeros
 
-def getProbsOutSymbols( Pinitial: list, channel: list[list]) -> list:
-    result = [ 0.0 ] * len(channel[0])
-
+def getProbsOutSymbols(Pinitial: list, channel: list[list]) -> list:
+    result = [0.0] * len(channel[0])
     for j in range(len(channel[0])):
         for i in range(len(channel)):
-            result[j] += channel[i][j]*Pinitial[i]
-
+            result[j] += channel[i][j] * Pinitial[i]
     return result
 
-def getPosterioriMatrix( Pinitial: list, channel: list[list]):
-    result = getMatrixZeros( len(channel), len(channel[0]) )
-    outsSymbProbs = getProbsOutSymbols( Pinitial, channel )
-
+def getPosterioriMatrix(Pinitial: list, channel: list[list]):
+    result = getMatrixZeros(len(channel), len(channel[0]))
+    outsSymbProbs = getProbsOutSymbols(Pinitial, channel)
     for i in range(len(channel)):
         for j in range(len(channel[0])):
             result[i][j] = channel[i][j]
-
             if outsSymbProbs[j] != 0:
-                result[i][j] *= Pinitial[i]/outsSymbProbs[j]
-    
+                result[i][j] *= Pinitial[i] / outsSymbProbs[j]
     return result
 
-def getMatrixSimultaneusEvent( Pinitial: list, channel: list[list]):
-    result = getMatrixZeros( len(channel), len(channel[0]) )
-
-    for i in range(len(channel)):
-        for j in range(len(channel[0])):
-            result[i][j] = channel[i][j] * Pinitial[i]
-    
+def getMatrixSimultaneusEvent(Pinitial: list, channel: list[list]):
+    rows = len(channel)
+    cols = len(channel[0])
+    result = getMatrixZeros(rows, cols)
+    for i in range(rows):
+        for j in range(cols):
+            result[i][j] = Pinitial[i] * channel[i][j]
     return result
